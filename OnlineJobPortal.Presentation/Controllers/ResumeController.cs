c4195b3b9e73114773c2fb8a55f4f9326eedacaa
dung-huynh001 <dungb1906640@gmail.com>
2023-11-03 03:36:22 +0700

    Update UploadCV Feature

--- OnlineJobPortal.Presentation/Controllers/ResumeController.cs
+++ OnlineJobPortal.Presentation/Controllers/ResumeController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using IronPdf;
+using System.IO;
+using IronPdf.Rendering;
+using System.Text;
 
 namespace OnlineJobPortal.Presentation.Controllers
 {
@@ -19,6 +23,54 @@ namespace OnlineJobPortal.Presentation.Controllers
         public IActionResult EditResume()
         {
             return View();
+        }
+
+        public IActionResult UploadCV(string htmlContent)
+        {
+            HtmlToPdf Renderer = new HtmlToPdf();
+
+            // Renderer.PrintOptions.SetCustomPaperSizeInInches(8.5, 11);
+            Renderer.PrintOptions.PaperSize = PdfPrintOptions.PdfPaperSize.A4;
+            Renderer.PrintOptions.PaperOrientation = IronPdf.Rendering.PdfPaperOrientation.Portrait;
+            Renderer.PrintOptions.Title = "Download PDF CV";
+
+            // Renderer.PrintOptions.RenderDelay = 50; //ms
+            // Renderer.PrintOptions.CssMediaType = PdfPrintOptions.PdfCssMediaType.Screen;
+            // Renderer.PrintOptions.DPI = 300;
+            // Renderer.PrintOptions.JpegQuality = 80;
+            // Renderer.PrintOptions.GrayScale = false;
+            // Renderer.PrintOptions.InputEncoding = Encoding.UTF8;
+            // Renderer.PrintOptions.Zoom = 100;
+            // Renderer.PrintOptions.ViewPortWidth = 1280;
+            Renderer.PrintOptions.CreatePdfFormsFromHtml = true;
+            Renderer.PrintOptions.CustomCssUrl = ".\\wwwroot\\css\\ResumePage\\main.css";
+            Renderer.PrintOptions.MarginTop = 0;
+            Renderer.PrintOptions.MarginLeft = 0;
+            Renderer.PrintOptions.MarginRight = 0;
+            Renderer.PrintOptions.MarginBottom = 0;
+            Renderer.PrintOptions.FirstPageNumber = 1;
+
+            var pdf = Renderer.RenderHtmlAsPdf(htmlContent);
+
+            string uploadsPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Uploads/CVs");
+
+
+            if (!Directory.Exists(uploadsPath))
+            {
+                Directory.CreateDirectory(uploadsPath);
+            }
+
+            string pdfFileName = Guid.NewGuid().ToString() + "_cv.pdf";
+            string pdfFilePath = Path.Combine(uploadsPath, pdfFileName);
+
+            System.IO.File.WriteAllBytes(pdfFilePath, pdf.BinaryData);
+
+
+            var contentLength = pdf.BinaryData.Length;
+
+            Response.Headers["Content-Length"] = contentLength.ToString();
+            Response.Headers.Add("Content-Disposition", "inline; filename=cv.pdf");
+            return File(pdf.BinaryData, "application/pdf");
         }
     }
 }
